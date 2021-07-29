2d95f9af9607b1eecb4e59ca5fbf44b191a8a31c
sarinsky <56095336+sarinsky@users.noreply.github.com>
2021-07-29 11:19:56 -0500

    Downloading files

---
+++ src/main/java/com/trnetwork/ARestFFinApplication.java
@@ -0,0 +1,13 @@
+package com.trnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARestFFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ARestFFinApplication.class, args);
+	}
+
+}
